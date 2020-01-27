931875a1e57dbbf917b60d48c9c719a54ec5bb83
zozombi <37424297+zozombi@users.noreply.github.com>
2020-01-27 21:05:07 +0100

    Updates.

--- bt-light.cpp
+++ bt-light.cpp
@@ -10,13 +10,21 @@
 using namespace pxt;
 MicroBitUARTService *uart;
 
+
+
 namespace BTlight {
-    /*
-    *
-    */
+    // #define MICROBIT_SD_GATT_TABLE_SIZE             0x500
+    
+
+
+    //%
+    void BT_init(void) {
+        uart = new MicroBitUARTService(*uBit.ble, 32, 32); 
+        uBit.display.scroll("AVM");    
+    }
+
     //%
     uint16_t BT_send(int16_t ADCVal) {
-
         return ADCVal * .976;
     }
 } 
--- bt-light.ts
+++ bt-light.ts
@@ -16,6 +16,10 @@ namespace BTlight {
     /**
     *
     */
+    //% weight=30 blockId="BTlight_init" block="init UART"
+    export function BTinit() {
+        BTlight.BTinit();
+    }
     //% weight=30 blockId="BTlight_send" block="send UART"
     export function BTsend(packet: number): number {
         return 2;
@@ -29,4 +33,8 @@ namespace BTlight {
 function BT_send(ADCVal: number) {
     // Fake function for simulator
     return 0
+}
+//% shim=BTlight::init
+function BT_init() {
+    // Fake function for simulator
 }
