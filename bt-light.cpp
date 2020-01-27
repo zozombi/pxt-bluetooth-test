ab15f74cd2052dfea6a86a35b156d7f66d503ec1
zozombi <37424297+zozombi@users.noreply.github.com>
2020-01-27 21:24:31 +0100

    Updates.

--- bt-light.cpp
+++ bt-light.cpp
@@ -13,9 +13,9 @@ MicroBitUARTService *uart;
 namespace BTlight {
     //%
     uint16_t BT_init(int x) {
-        // uart = new MicroBitUARTService(*uBit.ble, 32, 32); 
-        // uBit.display.scroll("AVM");    
-        // release_fiber();
+        uart = new MicroBitUARTService(*uBit.ble, 32, 32); 
+        uBit.display.scroll("AVM");    
+        //release_fiber();
         return 111111;
     }
 
